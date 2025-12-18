d97ea099a4ebb8b78cfa68c52945b8352541f489
hagiksm <hagiksm448@gmail.com>
2025-12-18 08:32:11 +0800

    menambahkan input baru

--- products.py
+++ products.py
@@ -1,23 +1,25 @@
 # products.py
 def tambah_barang():
-    print("-----INPUT BARANG-----")
-    kode = input("Masukkan kode barang: ")
-    nama = input("Masukkan nama barang: ")
-    harga = int(input("Masukkan harga barang: "))
-
-
     keranjang = {
-        "A001": {"nama": "Air Mineral", "harga": 5000},
-        "A002": {"nama": "Roti Coklat", "harga": 6000},
-        "A003": {"nama": "Mie Instan", "harga": 3500},
-        "A004": {"nama": "Kopi Botol", "harga": 12000},
-        "A005": {"nama": "Coklat Batang", "harga": 10000}
+       "M001": {"nama": "Nasi Goreng", "harga": 15000},
+        "M002": {"nama": "Mie Ayam", "harga": 12000},
+        "M003": {"nama": "Bakso", "harga": 13000},
+        "M004": {"nama": "Sate Ayam", "harga": 20000},
+        "M005": {"nama": "Gado-gado", "harga": 10000},
+        "M006": {"nama": "Ayam Penyet", "harga": 18000},
+        "M007": {"nama": "Rendang", "harga": 25000}
     }
+    while True:
+        print("-----INPUT BARANG-----")
+        kode = input("Masukkan kode barang: ")
+        nama = input("Masukkan nama barang: ")
+        harga = int(input("Masukkan harga barang: "))
 
-    keranjang[kode] = {"nama": nama, "harga": harga}
-
+        keranjang[kode] = {"nama": nama, "harga": harga}
+        lanjut = input("tambah barang lagi? (y/n): ")
+        if lanjut != 'y':
+            break
     return keranjang
 
-
 if __name__ == "__main__":
-    data = tambah_barang()
+    tambah_barang()
