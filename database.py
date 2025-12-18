601d5756c4545c1b2c139fb64bda01008d294dac
Andika Pradnyana <andikapradnyana16@gmail.com>
2025-12-18 11:10:21 +0800

    menyelesaikan logika database

--- database.py
+++ database.py
@@ -1,5 +1,13 @@
 import sqlite3
 
-def connect_db():
-    connection = sqlite3.connect("app.db")
+def main():
+    connection = sqlite3.connect("app.db")   
+
+    cursor = connection.cursor()
+
+    cursor.execute("CREATE TABLE IF NOT EXISTS products (id integer primary key, nama_produk varchar(100), jumlah_produk integer, harga integer)")
+
     return connection 
+
+if __name__ == "__main__":
+    main()
